7aaddafa3e9f35e6936df8bbf2c8f8953a71aec9
AwaisMajeed2536 <awais.m2536@gmail.com>
2017-12-18 00:52:24 -0800

    merger

--- app/src/main/java/com/example/bisma/calendar_analyzer/ui/MainActivity.java
+++ app/src/main/java/com/example/bisma/calendar_analyzer/ui/MainActivity.java
@@ -24,6 +24,7 @@ import android.widget.TextView;
 import com.example.bisma.calendar_analyzer.R;
 import com.example.bisma.calendar_analyzer.adapters.TodaysTasksAdapter;
 import com.example.bisma.calendar_analyzer.db.source.TasksSource;
+import com.example.bisma.calendar_analyzer.helpers.BottomNavigationHelper;
 import com.example.bisma.calendar_analyzer.helpers.Constants;
 import com.example.bisma.calendar_analyzer.helpers.UtilHelpers;
 import com.example.bisma.calendar_analyzer.models.EventModelDep;
@@ -31,75 +32,25 @@ import com.example.bisma.calendar_analyzer.models.EventModelDep;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
-
-    private MenuItem mSearchAction;
-
-
-    public boolean onPrepareOptions(Menu menu) {
-        mSearchAction = menu.findItem(R.id.action_search);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+public class MainActivity extends AppCompatActivity implements BottomNavigationHelper.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
-
+        BottomNavigationHelper.with(this).setCallBack(this);
         startActivityForResult(new Intent(this, GoogleApi.class), Constants.GOOGLE_API_GET_CALL_KEY);
-
-       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -122,48 +73,26 @@ public class MainActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
+    @Override
+    public void onSelectItem(int position) {
+        Fragment fragment;
+        switch (position) {
+            case 0:
+                fragment =  new CalendarView();
+                break;
+            case 1:
+                fragment =  new Analyzer();
+                break;
+            case 2:
+                fragment =  new History();
+                break;
+            case 3:
+                fragment =  new History();
+                break;
+            default:
+                fragment =  new CalendarView();
         }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    return new CalendarView();
-                case 1:
-                    return new Analyzer();
-                case 2:
-                    return new History();
-                default:
-                    return new CalendarView();
-            }
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Calendar";
-                case 1:
-                    return "Analyzer";
-                case 2:
-                    return "Reports";
-            }
-            return null;
-        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, fragment).commit();
     }
 
 
