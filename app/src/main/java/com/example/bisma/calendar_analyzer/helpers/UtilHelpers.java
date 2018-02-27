2532763ec85db8331143deaa03042f4b9ab22560
AwaisMajeed <awais.majeed@therightsw.com>
2018-02-27 23:04:06 +0500

    Tasks source edited

--- app/src/main/java/com/example/bisma/calendar_analyzer/helpers/UtilHelpers.java
+++ app/src/main/java/com/example/bisma/calendar_analyzer/helpers/UtilHelpers.java
@@ -177,7 +177,9 @@ public class UtilHelpers {
     }
 
     public static int getIdFromDate(String date) {
-        return (int) getDateInFormat(date).getTimeInMillis();
+        Calendar cal = getDateInFormat(date);
+        return cal.get(Calendar.YEAR) + cal.get(Calendar.MONTH) + cal.get(Calendar.DAY_OF_MONTH)
+                + cal.get(Calendar.HOUR) + cal.get(Calendar.MINUTE) + cal.get(Calendar.SECOND);
     }
 
     public static Calendar getDateInFormat(String date) {
